f291b646d4d11b83e134486026f5e545e3efc520
Eric Schmidt <ericdschmidt@gmail.com>
2021-03-22 10:36:45 -0400

    readme, docs, const

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # harmony-fsm
 A flexible and simple finite state machine in C++
+
+## Basic State Machine Operation
+
+A finite state machine is great way to enforce and allow only specified transitions between operating conditions in your software. You are much less likely to end up with undefined behavior than using what often turns into large switch/case statements or nested code. In harmony-fsm, these transitions are defined in tables with rows defining all the permutations of:
+current state -> event -> resultant state
+
+```C++
+
+enum class EVENT
+{
+  DO_NEXT_CYCLE,
+  EMERGENCY_DECLARED,
+  EMERGENCY_ENDED
+};
+
+enum class RUNSTATE
+{
+  GREEN,
+  YELLOW,
+  RED,
+  EMERGENCY
+};
+
+enum class RUNRESULT
+{
+  CYCLE_COMPLETE,
+  CYCLE_RUNNING,
+  FAILED
+};
+
+static const std::vector< fsm::EventTableEntry< EVENT, RUNSTATE > > STOPLIGHT_FSM_TABLE = {
+    // basic operation
+    { EVENT::DO_NEXT_CYCLE, RUNSTATE::GREEN, RUNSTATE::YELLOW },
+    { EVENT::DO_NEXT_CYCLE, RUNSTATE::YELLOW, RUNSTATE::RED },
+    { EVENT::DO_NEXT_CYCLE, RUNSTATE::RED, RUNSTATE::GREEN },
+
+    // emergency vehicle flashing, can happen at any time
+    { EVENT::EMERGENCY_DECLARED, RUNSTATE::GREEN, RUNSTATE::EMERGENCY },
+    { EVENT::EMERGENCY_DECLARED, RUNSTATE::YELLOW, RUNSTATE::EMERGENCY },
+    { EVENT::EMERGENCY_DECLARED, RUNSTATE::RED, RUNSTATE::EMERGENCY },
+
+    // back to red when emergency is done
+    { EVENT::EMERGENCY_ENDED, RUNSTATE::EMERGENCY, RUNSTATE::RED } };
+
+```
+
+Once constructed, you can call FiniteStateMachine::doEvent( const TEvent& trigger ) to evaluate whether a transition is valid and to switch states. 
+
+```C++
+// given machine.getCurrentState() == RUNSTATE::RED
+if ( machine.doEvent( EVENT::DO_NEXT_CYCLE ) )
+{
+  ASSERT( machine.getCurrentState() == RUNSTATE::GREEN );
+}
+
+```
+
+## Advanced State Management - FiniteStateMachineRunner
+
+The FiniteStateMachineRunner expands on the simplistic model to provide a threaded worker-watchdog model and allows you to run a system at a fixed frequency. THe watchdog thread watches for execution times that exceed a given threshold and handles the results returned from execution and calls for state changes. The worker thread executes the correct function for each state. States and their related functions can be managed either by a map of function pointers, or pass through a common execution branch (switch/case). You can optionally handle exceptions from these functions in the runner.
+
+See the unit tests for examples.
+
+## ROS Support
+
+If you configure cmake with "ROS_TIME" enabled, you can use ros::Time as the clock for loop rate management and timeouts. Otherwise, std::chrono is used.
--- include/harmony-fsm/finite_state_machine.hpp
+++ include/harmony-fsm/finite_state_machine.hpp
@@ -6,23 +6,30 @@
  * 
  * @copyright Copyright (c) 2021
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This is free and unencumbered software released into the public domain.
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <https://unlicense.org>
  */
 
 #pragma once
@@ -44,6 +51,13 @@ class FiniteStateMachine
 {
  public:
   FiniteStateMachine( const FiniteStateMachine& other ) = default;
+
+  /**
+   * @brief Construct a new Finite State Machine object.
+   * 
+   * @param fsm_table 
+   * @param init_state 
+   */
   FiniteStateMachine( std::vector< EventTableEntry< TEvent, TState > > fsm_table, TState init_state )
     : current_state_( init_state )
     , fsm_table_( fsm_table )
@@ -54,7 +68,27 @@ class FiniteStateMachine
    * @param trigger
    * @return true if the state change was executed successfully
    */
-  virtual bool doEvent( TEvent trigger )
+  virtual bool doEvent( const TEvent& trigger )
+  {
+    TState res;
+    if ( isValid( trigger, res ) )
+    {
+      current_state_ = res;
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * @brief Checks whether the current transition event could yield a new state
+   * 
+   * @param trigger 
+   * @param next_state The next state given this transition
+   * @return true 
+   * @return false 
+   */
+  virtual bool isValid( const TEvent& trigger, TState& next_state )
   {
     for ( const auto& entry : fsm_table_ )
     {
@@ -62,7 +96,7 @@ class FiniteStateMachine
       {
         if ( entry.Trigger == trigger )
         {
-          current_state_ = entry.Result;
+          next_state = entry.Result;
           return true;
         }
       }
